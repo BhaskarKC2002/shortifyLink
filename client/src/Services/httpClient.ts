1e542753140b6ce67461f3bd409ff8836403b04b
Bhaskar KC <kcbhaskar1328@gmail.com>
2025-04-13 21:29:11 +0545

    Fix API URL in httpClient

--- client/src/Services/httpClient.ts
+++ client/src/Services/httpClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { handleRefreshToken } from "./authServices";
 
-axios.defaults.baseURL = "http://localhost:5001/api/";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5001/api/";
+axios.defaults.baseURL = API_URL;
 
 axios.interceptors.request.use(
   function (config) {
